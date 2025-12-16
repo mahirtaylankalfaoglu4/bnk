86c324dc05973e8d94dcaa6c174cb6377add9a30
mahir <mahirtaylankalfaoglu@gmail.com>
2025-12-16 18:19:04 +0300

    Remove all existing files

--- hesap_hareketleri.js
+++ hesap_hareketleri.js
@@ -1,6 +1,13 @@
 
-// Hesap Hareketleri - Güncelleme: 2025-12-16 17:25:56
-const hesapHareketleri = [
+// Hesap Hareketleri - Güncelleme: 2025-12-16 18:14:35
+export const hesapHareketleri = [
+  [
+    "16\nARALIK\n2025",
+    "FAST5731839389-MAHİR TAYLAN KALFAOĞLU-",
+    "Para Transferi\nDekont",
+    "+1,00 TL",
+    "17,00 TL"
+  ],
   [
     "16\nARALIK\n2025",
     "FAST5727827414-MAHİR TAYLAN KALFAOĞLU-",
@@ -44,11 +51,4 @@ const hesapHareketleri = [
     "1,00 TL"
   ]
 ];
-
-// Veriye erişim fonksiyonu
-function getHesapHareketleri() {
-    return hesapHareketleri;
-}
-
-// Son güncelleme zamanı
-const sonGuncelleme = "2025-12-16 17:25:56";
+export const sonGuncelleme = "2025-12-16 18:14:35";
