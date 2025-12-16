4b260722a6d6822c4e1342f20c376b4d9730da2e
mahir <mahirtaylankalfaoglu@gmail.com>
2025-12-16 18:00:25 +0300

    Update project files with hesap_hareketleri and scripts5

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-// Tablo elementlerini seç
 const tableBody = document.getElementById("tableBody");
 const totalBalance = document.getElementById("totalBalance");
 const lastUpdate = document.getElementById("lastUpdate");
 
-// GitHub raw URL
+// GitHub raw JS dosya URL'si
 const url = "https://raw.githubusercontent.com/mahirtaylankalfaoglu4/bnk/main/hesap_hareketleri.js";
 
-// Veriyi fetch ile çek
 fetch(url)
     .then(response => response.text())
-    .then(data => {
-        // JS kodunu çalıştır (hesapHareketleri ve sonGuncelleme değişkenlerini al)
-        eval(data); // Burada hesapHareketleri ve sonGuncelleme tanımlanmış olacak
+    .then(jsCode => {
+        // JS kodunu çalıştırarak hesapHareketleri ve sonGuncelleme değişkenlerini oluştur
+        eval(jsCode);
 
         // Tabloyu temizle
         tableBody.innerHTML = "";
